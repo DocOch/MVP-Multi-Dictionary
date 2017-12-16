4c8fa1c12612869a1f998b43b5cf9ed934cb8ca1
DocOch <Ochmanowicz@gmail.com>
2017-12-16 04:05:38 -0500

    setting state in placeholder for entry

--- src/Client/Entry.js
+++ src/Client/Entry.js
@@ -1,14 +1,21 @@
 import React from 'react';
 
 class Entry extends React.Component {
-
+  constructor(props){
+	super(props)
+	this.state = {
+	  entry: "your definition here"
+	}
+  }
   render(){
   	return (
   	  <div>
-  	  <p>in Entry</p>
+  	  <textarea rows="6" cols="75" placeholder={this.state.entry ? this.state.entry : "definition"}></textarea>
   	  </div>
   	)
   }
 }
 
 export default Entry;
+// placeholder={"" + this.state.example}
+// <textarea rows={unreadMessages.length > 0 && 8} cols="15"></textarea>
